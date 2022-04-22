0fb48432803f6e59429a4d9de51efa1240d9bb61
mpcarolin <mpcarolin.dev@gmail.com>
2022-04-22 15:26:03 -0700

    GROK-1627: Add methods for collection classes
    - Add sv-graphql-client dependency
    - Implement find,insert,remove for movies.js and people.js

--- src/prefixes/collections/movies.js
+++ src/prefixes/collections/movies.js
@@ -1,6 +1,9 @@
+const {
+  query,
+  nullToUndefined
+} = require('@simpleview/sv-graphql-client')
+
 class Movies {
-  _graphUrl = null
-  _graphServer = null
 
   constructor ({ graphUrl, graphServer }) {
     this._graphUrl = graphUrl 
@@ -8,15 +11,60 @@ class Movies {
   }
 
   async find ({ fields, headers, context, filter }) {
+    const results = await query({
+      url: this._graphUrl,
+      query: `query FindMovies($filter: training_movies_find_input) {
+        training {
+          training_movies_find(filter: $filter) {
+            ${fields}
+          }
+        }
+      }`,
+      headers,
+      variables: { filter } 
+    })
 
+    nullToUndefined(results) // mutates results in-place
+
+    return results
   }
 
   async insert ({ fields, headers, context, input }) {
+    const results = await query({
+      url: this._graphUrl,
+      query: `mutation InsertMovies($input: [training_movies_insert_input!]!) {
+        training {
+          training_movies_insert(input: $input) {
+            ${fields}
+          }
+        }
+      }`,
+      headers,
+      variables: { input } 
+    })
 
+    nullToUndefined(results) // mutates results in-place
+
+    return results
   }
 
   async remove ({ fields, headers, context, filter }) {
+    const results = await query({
+      url: this._graphUrl,
+      query: `mutation RemoveMovies($filter: training_movies_remove_input) {
+        training {
+          training_movies_remove(filter: $filter) {
+            ${fields}
+          }
+        }
+      }`,
+      headers,
+      variables: { filter } 
+    })
 
+    nullToUndefined(results) // mutates results in-place
+
+    return results
   }
 }
 
--- src/prefixes/collections/people.js
+++ src/prefixes/collections/people.js
@@ -1,6 +1,9 @@
+const {
+  query,
+  nullToUndefined
+} = require('@simpleview/sv-graphql-client')
+
 class People {
-  _graphUrl = null
-  _graphServer = null
 
   constructor ({ graphUrl, graphServer }) {
     this._graphUrl = graphUrl 
@@ -8,16 +11,63 @@ class People {
   }
 
   async find ({ fields, headers, context, filter }) {
+    const results = await query({
+      url: this._graphUrl,
+      query: `query FindPeople($filter: training_people_find_input) {
+        training {
+          training_people_find(filter: $filter) {
+            ${fields}
+          }
+        }
+      }`,
+      headers,
+      variables: { filter } 
+    })
 
+    nullToUndefined(results) // mutates results in-place
+
+    return results
   }
 
   async insert ({ fields, headers, context, input }) {
+    const results = await query({
+      url: this._graphUrl,
+      query: `mutation InsertPeople($input: [training_people_insert_input!]!) {
+        training {
+          training_people_insert(input: $input) {
+            ${fields}
+          }
+        }
+      }`,
+      headers,
+      variables: { input } 
+    })
 
+    nullToUndefined(results) // mutates results in-place
+
+    return results
   }
 
   async remove ({ fields, headers, context, filter }) {
+    const results = await query({
+      url: this._graphUrl,
+      query: `mutation RemovePeople($filter: training_people_remove_input) {
+        training {
+          training_people_remove(filter: $filter) {
+            ${fields}
+          }
+        }
+      }`,
+      headers,
+      variables: { filter } 
+    })
 
+    nullToUndefined(results) // mutates results in-place
+
+    return results
   }
+
 }
+
 
 module.exports = { People }
